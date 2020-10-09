a643472669bcdb0741fb44d2f0da9780b618ce00
kusohyeon <kusohyeon@gmail.com>
2020-10-09 20:26:58 +0900

    .

--- Back/ProductMgr/src/com/ssafy/model/dao/LoginDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.model.dao;
-
-import com.ssafy.model.MemberDto;
-
-public interface LoginDAO {
-
-	MemberDto login(MemberDto member) throws Exception;
-
-}
--- Back/ProductMgr/src/com/ssafy/model/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.model.service;
-
-import com.ssafy.model.MemberDto;
-
-public interface LoginService {
-	MemberDto login(MemberDto member) throws Exception;
-}
